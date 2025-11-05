40ac3cac3b549ca2c024bd9fff1ab54976d041c5
MajoSepulveda <maria.sepulveda59886@ucaldas.edu.co>
2025-11-05 15:27:40 -0500

    métodos para agregar y quitar saldo agregados

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -1,19 +1,22 @@
-package domain;
+package src.domain;
 import java.io.Serializable;
 
 public class Customer implements Serializable {
     private String name;
-    private int ID;
+    private int id;
     private float balance;
 
-    public Customer(String name, int ID, float balance){
-        this.ID = ID;
+    public Customer(String name, int id, float balance){
+        if (id < 0){
+            throw new IllegalArgumentException("El ID del cliente no puede ser negativo.");
+        }
+        this.id = id;
         setName(name);
         setBalance(balance);
     }
 
-    public int getID(){
-        return ID;
+    public int getId(){
+        return id;
     }
 
     public String getName(){
@@ -36,5 +39,22 @@ public class Customer implements Serializable {
             throw new IllegalArgumentException("El saldo del cliente no puede ser negativo.");
         }
         this.balance = balance;
+    }
+
+    public void reduceBalance(float amount){
+        if (amount < 0){
+            throw new IllegalArgumentException("El monto a reducir no puede ser negativo.");
+        }
+        else if (amount > balance){
+            throw new IllegalArgumentException("El monto a reducir no puede ser mayor que el saldo actual.");
+        }
+        balance -= amount;
+    }
+
+    public void addBalance(float amount){
+        if (amount < 0){
+            throw new IllegalArgumentException("El monto a agregar no puede ser negativo.");
+        } 
+        balance += amount;
     }
 }
